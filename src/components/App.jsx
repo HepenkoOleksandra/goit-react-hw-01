cc86d307f1a3357ae1866862c2fc8d65ed49a07a
HepenkoOleksandra <gepenkoalex@gmail.com>
2024-03-11 23:46:40 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import Profile from './Profile/Profile';
 import userData from '../userData.json';
 import friends from '../friends.json';
 import FriendList from './FriendList/FriendList';
+import { Alert } from './books';
 
 const App = () => {
   return (<>
@@ -13,6 +14,20 @@ const App = () => {
       stats={userData.stats}
     />
     <FriendList friends={friends} /> 
+      <>
+      <Alert variant="info">
+        Would you like to browse our recommended products?
+      </Alert>
+      <Alert variant="error">
+        There was an error during your last transaction
+      </Alert>
+      <Alert variant="success">
+        Payment received, thank you for your purchase
+      </Alert>
+      <Alert variant="warning">
+        Please update your profile contact information
+      </Alert>
+    </>
     </>
   )
 }
